2e102834f0ba9ec78ae16f4f2c9ba30f77ab6fcd
aosoft <tan-y@aosoft.jp>
2020-03-30 23:35:46 +0900

    Add MucomDotNetEncoding, MucomDotNetUtility

---
+++ Scripts/MucomDotNetEncoding.cs
@@ -0,0 +1,36 @@
+﻿using mucomDotNET.Common;
+using System;
+using System.Text;
+
+namespace MucomUnity
+{
+    public class MucomDotNetEncoding : iEncoding
+    {
+        private static Lazy<MucomDotNetEncoding> defaultEncoding;
+
+        static MucomDotNetEncoding()
+        {
+            defaultEncoding = new Lazy<MucomDotNetEncoding>(() => new MucomDotNetEncoding(), true);
+        }
+
+        public static iEncoding Default => defaultEncoding.Value;
+
+#if UNITY_EDITOR
+        byte[] iEncoding.GetSjisArrayFromString(string utfString) => myEncoding.Default.GetSjisArrayFromString(utfString);
+
+        string iEncoding.GetStringFromSjisArray(byte[] sjisArray) => myEncoding.Default.GetStringFromSjisArray(sjisArray);
+
+        string iEncoding.GetStringFromSjisArray(byte[] sjisArray, int index, int count) => myEncoding.Default.GetStringFromSjisArray(sjisArray, index, count);
+#else
+        byte[] iEncoding.GetSjisArrayFromString(string utfString) => Encoding.UTF8.GetBytes(utfString);
+
+        string iEncoding.GetStringFromSjisArray(byte[] sjisArray) => Encoding.UTF8.GetString(sjisArray);
+
+        string iEncoding.GetStringFromSjisArray(byte[] sjisArray, int index, int count) => Encoding.UTF8.GetString(sjisArray, index, count);
+#endif
+
+        string iEncoding.GetStringFromUtfArray(byte[] utfArray) => Encoding.UTF8.GetString(utfArray);
+
+        byte[] iEncoding.GetUtfArrayFromString(string utfString) => Encoding.UTF8.GetBytes(utfString);
+    }
+}
---
+++ Scripts/MucomDotNetUtility.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using mucomDotNET.Compiler;
+using UnityEngine;
+
+using MucomLog = musicDriverInterface.Log;
+using MucomLogLevel = musicDriverInterface.LogLevel;
+
+namespace MucomUnity
+{
+    public static class MucomDotNetUtility
+    {
+		public static void InitializeMucomLogger(bool setUnityLogger)
+		{
+			MucomLog.writeLine = setUnityLogger ? MucomLog_WriteLine : (System.Action<MucomLogLevel, string>)null;
+		}
+
+		public static Compiler CreateCompiler()
+		{
+			var ret = new Compiler(MucomDotNetEncoding.Default);
+			ret.Init();
+			return ret;
+		}
+
+		private static void MucomLog_WriteLine(MucomLogLevel level, string msg)
+		{
+			if ((level & (MucomLogLevel.FATAL | MucomLogLevel.ERROR)) != 0)
+			{
+				Debug.LogError(msg);
+			}
+			else if ((level & MucomLogLevel.WARNING) != 0)
+			{
+				Debug.LogWarning(msg);
+			}
+			else
+			{
+				Debug.Log(msg);
+			}
+		}
+
+		public static Stream OpenFromStreamingAssets(string fileName)
+		{
+#if UNITY_ANDROID
+			using (var www = new UnityEngine.Networking.UnityWebRequest(Path.Combine(Application.streamingAssetsPath, fileName)))
+			{
+				www.SendWebRequest();
+				while (!www.isDone) ;
+				var ret = new MemoryStream(www.downloadHandler.data.Length);
+				ret.Write(www.downloadHandler.data, 0, www.downloadHandler.data.Length);
+				ret.Seek(0, SeekOrigin.Begin);
+				return ret;
+			}
+#else
+			return new FileStream(Path.Combine(Application.streamingAssetsPath, fileName), FileMode.Open, FileAccess.Read);
+#endif
+		}
+	}
+}
